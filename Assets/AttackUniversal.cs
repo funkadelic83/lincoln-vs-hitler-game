38373ceeb802df115f57d47b1b5a919601c915a9
Mike Damanskis <mikedamanskis@gmail.com>
2021-09-13 19:15:51 -0700

    remove debug log

--- Assets/AttackUniversal.cs
+++ Assets/AttackUniversal.cs
@@ -26,7 +26,6 @@ public class AttackUniversal : MonoBehaviour
         if (hit.Length == 0)
         {
             attackMissed = true;
-            Debug.Log("Missed!");
         }
     
         if (hit.Length > 0)
@@ -66,13 +65,11 @@ public class AttackUniversal : MonoBehaviour
 
                 if (gameObject.CompareTag(Tags.LEFT_ARM_TAG) || gameObject.CompareTag(Tags.LEFT_LEG_TAG))
                 {
-                    //Debug.Log(hit[0].name);
-                    hit[0].GetComponent<HealthScript>().ApplyDamage(damage, true);
+                    hit[0].GetComponent<HealthScript>().ApplyDamage(damage, false);
                 }
                 else
                 {
-                    //Debug.Log(hit[0].name);
-                    hit[0].GetComponent<HealthScript>().ApplyDamage(2f * damage, false);
+                    hit[0].GetComponent<HealthScript>().ApplyDamage(2f * damage, true);
                 }
             } 
 
